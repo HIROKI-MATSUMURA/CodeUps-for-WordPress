9d97dc2c8c1a999b32f7c6dac5b94b0da91e1ede
HIROKI-MATSUMURA <hiroki@codeups.jp>
2023-08-03 19:00:24 +0900

    front-page.phpとhome.phpを設定しよう

---
+++ WordPressTheme/home.php
@@ -0,0 +1,124 @@
+<?php get_header(); ?>
+
+  <!-- 下層ページのメインビュー -->
+  <div class="p-sub-mv js-mv-height">
+    <div class="p-sub-mv__title">
+      <div class="c-section-header--white">
+        <h1 class="c-section-header__engtitle">news</h1>
+        <p class="c-section-header__jatitle">お知らせ</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- パンくず -->
+  <div class="c-breadcrumb">
+    <div class="l-inner">
+      <div>HOME > NEWS</div>
+    </div>
+  </div>
+
+  <div class="l-archive-home p-archive-home">
+    <div class="p-archive-home__inner l-inner">
+      <!-- ループ処理開始 -->
+      <div class="p-archive-home__item p-news-content">
+        <div class="p-news-content__meta">
+          <time datetime="2022-03-04" class="p-news-content__date">2022.3.4</time>
+          <p class="p-news-content__category">お知らせ</p>
+        </div>
+        <div class="p-news-content__link">
+          <a href="" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
+        </div>
+      </div>
+      <!-- ループ終了 -->
+      <div class="p-archive-home__item p-news-content">
+        <div class="p-news-content__meta">
+          <time datetime="2022-03-04" class="p-news-content__date">2022.3.4</time>
+          <p class="p-news-content__category">お知らせ</p>
+        </div>
+        <div class="p-news-content__link">
+          <a href="" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
+        </div>
+      </div>
+      <div class="p-archive-home__item p-news-content">
+        <div class="p-news-content__meta">
+          <time datetime="2022-03-04" class="p-news-content__date">2022.3.4</time>
+          <p class="p-news-content__category">お知らせ</p>
+        </div>
+        <div class="p-news-content__link">
+          <a href="" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
+        </div>
+      </div>
+      <div class="p-archive-home__item p-news-content">
+        <div class="p-news-content__meta">
+          <time datetime="2022-03-04" class="p-news-content__date">2022.3.4</time>
+          <p class="p-news-content__category">お知らせ</p>
+        </div>
+        <div class="p-news-content__link">
+          <a href="" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
+        </div>
+      </div>
+      <div class="p-archive-home__item p-news-content">
+        <div class="p-news-content__meta">
+          <time datetime="2022-03-04" class="p-news-content__date">2022.3.4</time>
+          <p class="p-news-content__category">お知らせ</p>
+        </div>
+        <div class="p-news-content__link">
+          <a href="" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
+        </div>
+      </div>
+      <div class="p-archive-home__item p-news-content">
+        <div class="p-news-content__meta">
+          <time datetime="2022-03-04" class="p-news-content__date">2022.3.4</time>
+          <p class="p-news-content__category">お知らせ</p>
+        </div>
+        <div class="p-news-content__link">
+          <a href="" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
+        </div>
+      </div>
+      <div class="p-archive-home__item p-news-content">
+        <div class="p-news-content__meta">
+          <time datetime="2022-03-04" class="p-news-content__date">2022.3.4</time>
+          <p class="p-news-content__category">お知らせ</p>
+        </div>
+        <div class="p-news-content__link">
+          <a href="" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
+        </div>
+      </div>
+      <div class="p-archive-home__item p-news-content">
+        <div class="p-news-content__meta">
+          <time datetime="2022-03-04" class="p-news-content__date">2022.3.4</time>
+          <p class="p-news-content__category">お知らせ</p>
+        </div>
+        <div class="p-news-content__link">
+          <a href="" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
+        </div>
+      </div>
+      <div class="p-archive-home__item p-news-content">
+        <div class="p-news-content__meta">
+          <time datetime="2022-03-04" class="p-news-content__date">2022.3.4</time>
+          <p class="p-news-content__category">お知らせ</p>
+        </div>
+        <div class="p-news-content__link">
+          <a href="" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
+        </div>
+      </div>
+      <div class="p-archive-home__item p-news-content">
+        <div class="p-news-content__meta">
+          <time datetime="2022-03-04" class="p-news-content__date">2022.3.4</time>
+          <p class="p-news-content__category">お知らせ</p>
+        </div>
+        <div class="p-news-content__link">
+          <a href="" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="l-paginavi">
+    <div class="wp-pagenavi">
+      <span class="current">1</span>
+      <a class="page smaller" href="#">2</a>
+      <a class="page smaller" href="#">3</a>
+    </div>
+  </div>
+<?php get_footer(); ?>
